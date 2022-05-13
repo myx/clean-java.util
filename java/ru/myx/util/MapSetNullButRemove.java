6f1e3dfefc649852d193710a94b359d9d69ee522
Alexander Kharichev <myx@myx.co.nz>
2022-05-13 10:11:07 +0300

    * formatting

--- java/ru/myx/util/MapSetNullButRemove.java
+++ java/ru/myx/util/MapSetNullButRemove.java
@@ -1,6 +1,6 @@
 /*
  * Created on 25.11.2003
- * 
+ *
  * To change the template for this generated file go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
@@ -8,32 +8,41 @@ package ru.myx.util;
 
 import java.util.Map;
 
-/**
- * @author myx
- * 
+/** @author myx
+ *
  * @param <K>
- * @param <V>
- */
+ * @param <V> */
 public final class MapSetNullButRemove<K, V> extends MapWithDefaults<K, V> {
-	/**
-	 * @param values
-	 * @param defaults
-	 */
+
+	/** @param values
+	 * @param defaults */
 	public MapSetNullButRemove(final Map<K, V> values, final Map<K, V> defaults) {
-		super( values, defaults );
+
+		super(values, defaults);
 	}
-	
+
 	@Override
 	public final void clear() {
+
 		for (final Map.Entry<K, V> current : this.values.entrySet()) {
-			current.setValue( null );
+			current.setValue(null);
 		}
 	}
 	
 	@SuppressWarnings("unchecked")
 	@Override
 	public final V remove(final Object key) {
+
 		final K k = (K) key;
-		return this.values.put( k, null );
+		return this.values.put(k, null);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean remove(final Object key, final Object value) {
+		
+		final K k = (K) key;
+		final V v = (V) value;
+		return super.replace(k, v, null);
 	}
 }
