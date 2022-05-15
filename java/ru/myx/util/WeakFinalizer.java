9b3b7faaa08ed6cba05435b54ccda74e0c824bbb
Alexander Kharichev <myx@myx.ru>
2022-05-15 21:19:55 +0300

    * WeakFinaliser - remove dependency

--- java/ru/myx/util/WeakFinalizer.java
+++ java/ru/myx/util/WeakFinalizer.java
@@ -4,70 +4,72 @@ import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
 import java.util.function.Consumer;
 
-import ru.myx.ae3.report.Report;
-
-/** @author myx
+/**
+ * @author myx
  *
- * @param <T> */
+ * @param <T>
+ */
 public class WeakFinalizer<T> extends WeakReference<T> {
 
-	private static ReferenceQueue<Object> QUEUE = new ReferenceQueue<>();
+    private static ReferenceQueue<Object> QUEUE = new ReferenceQueue<>();
 
-	static {
-		new Thread("WEAK-FINALIZER") {
+    static {
+	new Thread("WEAK-FINALIZER") {
 
-			{
-				this.setDaemon(true);
-			}
+	    {
+		this.setDaemon(true);
+	    }
 
-			@Override
-			public void run() {
+	    @Override
+	    public void run() {
 
-				for (;;) {
-					WeakFinalizer<?> reference;
-					try {
-						reference = (WeakFinalizer<?>) WeakFinalizer.QUEUE.remove();
-					} catch (final InterruptedException e) {
-						System.err.println("Transfer " + this + " exits (interrupted).");
-						return;
-					}
-					if (reference == null) {
-						System.err.println("Transfer " + this + " exits (exhausted).");
-						return;
-					}
-					try {
-						reference.clean();
-					} catch (final Throwable t) {
-						Report.exception("DEFER-CLOSE-THREAD", "Close fail", t);
-					}
-				}
-			}
+		for (;;) {
+		    WeakFinalizer<?> reference;
+		    try {
+			reference = (WeakFinalizer<?>) WeakFinalizer.QUEUE.remove();
+		    } catch (final InterruptedException e) {
+			System.err.println("DEFER-CLOSE-THREAD: Transfer " + this + " exits (interrupted).");
+			return;
+		    }
+		    if (reference == null) {
+			System.err.println("DEFER-CLOSE-THREAD: Transfer " + this + " exits (exhausted).");
+			return;
+		    }
+		    try {
+			reference.clean();
+		    } catch (final Throwable t) {
+			System.err.println("DEFER-CLOSE-THREAD: Transfer " + this + " clean fail.");
+		    }
+		}
+	    }
 
-		}.start();
+	}.start();
 
-	}
+    }
 
-	/** @param <T>
-	 * @param t
-	 * @param f
-	 * @return */
-	@SuppressWarnings("unused")
-	public static <T> T register(final T t, final Consumer<T> f) {
+    /**
+     * @param <T>
+     * @param t
+     * @param f
+     * @return
+     */
+    @SuppressWarnings("unused")
+    public static <T> T register(final T t, final Consumer<T> f) {
 
-		new WeakFinalizer<>(t, f);
-		return t;
-	}
+	new WeakFinalizer<>(t, f);
+	return t;
+    }
 
-	final Consumer<T> f;
+    final Consumer<T> f;
 
-	private WeakFinalizer(final T t, final Consumer<T> f) {
+    private WeakFinalizer(final T t, final Consumer<T> f) {
 
-		super(t, WeakFinalizer.QUEUE);
-		this.f = f;
-	}
+	super(t, WeakFinalizer.QUEUE);
+	this.f = f;
+    }
 
-	private void clean() {
+    private void clean() {
 
-		this.f.accept(this.get());
-	}
+	this.f.accept(this.get());
+    }
 }
